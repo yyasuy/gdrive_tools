0225068e47bd4ede92b7b10064e35f2a5dd5a210
Yasuhiro Yamanaka <yyasuy@gmail.com>
2018-08-16 11:22:30 +0900

    Move the secret files to /var/www/auth/google.

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-$secret_json = json_decode( file_get_contents( '.secret.json' ), true );
+define( 'SECRET_FILE',          '/var/www/auth/google/.secret.json' );
+define( 'TOKEN_FILE',		'/var/www/auth/google/.token.json' );
+$secret_json = json_decode( file_get_contents( SECRET_FILE ), true );
 define( 'CLIENT_ID',		 $secret_json[ 'web' ][ 'client_id' ] );
 define( 'CLIENT_SECRET',	 $secret_json[ 'web' ][ 'client_secret' ] );
 define( 'AUTH_URL',		'https://accounts.google.com/o/oauth2/v2/auth' );
 define( 'REVOKE_URL',		'https://accounts.google.com/o/oauth2/revoke' );
-define( 'TOKEN_URL',		'https://www.googleapis.com/oauth2/v4/token' );
 define( 'SCOPE',		'https://www.googleapis.com/auth/drive' );
-define( 'TOKEN_FILE',		'.token.json' );
+define( 'TOKEN_URL',		'https://www.googleapis.com/oauth2/v4/token' );
 define( 'GDRIVE_FILES_URL',	'https://www.googleapis.com/drive/v3/files' );
 define( 'MIN_UPDATED_TIME',     '-1 days' );
 define( 'TEAM_DRIVE_ID',	'0ANgQhBypclFIUk9PVA' );
